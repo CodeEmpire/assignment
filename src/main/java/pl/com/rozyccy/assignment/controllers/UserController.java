5a18cf485d198a41e2d15f1e339c0c95579b696d
Marcin Różycki <rozyckimarcin@wp.pl>
2021-02-02 22:00:43 +0100

    Add User class

--- src/main/java/pl/com/rozyccy/assignment/controllers/UserController.java
+++ src/main/java/pl/com/rozyccy/assignment/controllers/UserController.java
@@ -1,16 +1,26 @@
 package pl.com.rozyccy.assignment.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
+import pl.com.rozyccy.assignment.domain.User;
+import pl.com.rozyccy.assignment.services.UsersService;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    UsersService usersService;
+
     @GetMapping("/users/{login}")
-    String getUser(@PathVariable String login) {
+    ResponseEntity<User> getUser(@PathVariable String login) throws JsonProcessingException {
         System.out.println("Endpoint requested. Parameter login is: " + login);
-        return "Hello " + login;
+        return new ResponseEntity<>(usersService.getUser(login), HttpStatus.OK);
     }
 
 }
---
+++ src/main/java/pl/com/rozyccy/assignment/domain/User.java
@@ -0,0 +1,21 @@
+package pl.com.rozyccy.assignment.domain;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.Data;
+
+import java.time.ZonedDateTime;
+
+@Data
+public class User {
+    long id;
+    String login;
+    String name;
+    String type;
+    @JsonAlias("avatar_url")
+    String avatarUrl;
+    @JsonAlias("created_at")
+    ZonedDateTime createdAt;
+    int followers;
+    int publicRepos;
+    double calculations;
+}
